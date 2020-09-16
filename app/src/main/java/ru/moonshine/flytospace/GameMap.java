21da505c678e1fb771222ef5284443fa5717a18e
Semyon <semen2k1@mail.ru>
2020-09-16 18:17:46 +0500

    add GameMap.java & start button animation

---
+++ app/src/main/java/ru/moonshine/flytospace/GameMap.java
@@ -0,0 +1,21 @@
+package ru.moonshine.flytospace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.pm.ActivityInfo;
+
+public class GameMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
--- app/src/main/java/ru/moonshine/flytospace/MainActivity.java
+++ app/src/main/java/ru/moonshine/flytospace/MainActivity.java
@@ -2,10 +2,17 @@ package ru.moonshine.flytospace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.content.pm.ActivityInfo;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        final Button btnScale = (Button)findViewById(R.id.btnStart);
+        btnScale.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(final View v) {
+                v.startAnimation(animScale);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        setContentView(R.layout.game_map);
+                    }
+                }, 250);
+            }
+        });
+    }
+
+    public void btnStartOnClick(View view) {
+        setContentView(R.layout.game_map);
     }
 }
