f26156ec81713f07ea6c20353c0ac959d2b53149
syomcha <syomcha@users.noreply.github.com>
2020-09-24 12:37:06 +0500

    added exit from MainMenu activity

---
+++ app/src/main/java/ru/moonshine/flytospace/MainMenu.java
@@ -0,0 +1,43 @@
+package ru.moonshine.flytospace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.exitTitle)
+                .setMessage(R.string.exitMessage)
+                .setNegativeButton(R.string.no, null)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).create().show();
+    }
+}
--- app/src/main/java/ru/moonshine/flytospace/StartScreen.java
+++ app/src/main/java/ru/moonshine/flytospace/StartScreen.java
@@ -2,43 +2,46 @@ package ru.moonshine.flytospace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.AnimatorSet;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.content.pm.ActivityInfo;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class StartScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_screen);
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        animation.setStartOffset(0);
+        animation.setDuration(1000);
+        ImageView bg = findViewById(R.id.bg);
+        bg.startAnimation(animation);
     }
 
-    public void btnStartOnClick(View view) {
-        Intent intent = new Intent(this, GameMap.class);
-//        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-
+    public void bgLayoutOnClick(View view) {
+        Intent intent = new Intent(this, MainMenu.class);
         ActivityOptions option  = ActivityOptions.makeCustomAnimation(this,
                 R.anim.fade_in, R.anim.fade_out);
+        /* Анимация кнопки, чтобы не забыть)))
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+            Button btnStart = findViewById(R.id.btnStart);
+            btnStart.startAnimation(animation);
+         */
         Bundle bundle = option.toBundle();
-
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
-        Button btnStart = findViewById(R.id.btnStart);
-        btnStart.startAnimation(animation);
         startActivity(intent, bundle);
+        finish();
     }
 }
