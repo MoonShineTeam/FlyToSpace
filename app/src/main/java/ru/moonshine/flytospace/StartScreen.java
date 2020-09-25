ebebc837f7672ea6af9ba7b1b7639cb30acf00ee
syomcha <syomcha@users.noreply.github.com>
2020-09-25 19:19:14 +0500

    designed main_menu + added items in values + main_menu logic

--- app/src/main/java/ru/moonshine/flytospace/StartScreen.java
+++ app/src/main/java/ru/moonshine/flytospace/StartScreen.java
@@ -7,14 +7,11 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class StartScreen extends AppCompatActivity {
 
