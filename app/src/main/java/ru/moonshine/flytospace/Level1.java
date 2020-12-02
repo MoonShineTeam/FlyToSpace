b21a78c11fd81e4e87c21072ee257fcd6210ae17
Semyon <semen2k1@mail.ru>
2020-12-02 20:16:45 +0500

    Created level1

---
+++ app/src/main/java/ru/moonshine/flytospace/Level1.java
@@ -0,0 +1,47 @@
+package ru.moonshine.flytospace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.moonshine.flytospace.source.Utils;
+
+import android.content.Context;
+import android.os.Bundle;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class Level1 extends AppCompatActivity {
+
+    public Level1() throws IOException, JSONException {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level1);
+        Utils.setFullScreenMode(this);
+    }
+
+    String jsonText = readText(this.getApplicationContext(), R.raw.levels);
+    JSONObject obj = new JSONObject(jsonText);
+
+
+
+    private static String readText(Context context, int resId) throws IOException {
+        InputStream is = context.getResources().openRawResource(resId);
+        BufferedReader br= new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb= new StringBuilder();
+        String s= null;
+        while((  s = br.readLine())!=null) {
+            sb.append(s);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+}
--- app/src/main/java/ru/moonshine/flytospace/model/Task.java
+++ app/src/main/java/ru/moonshine/flytospace/model/Task.java
@@ -3,7 +3,43 @@ package ru.moonshine.flytospace.model;
 import java.util.ArrayList;
 
 public class Task {
-    private static int score;
-    private static ArrayList<String> answers;
+    private final int id;
+    private final String taskText;
+    private final int time;
+    private final int score;
+    private final Boolean isOpen;
+    private final ArrayList<String> answers;
 
+    public Task(int id, String taskText, ArrayList<String> answers) {
+        this.id = id;
+        this.taskText = taskText;
+        this.answers = answers;
+        this.time = 0;
+        this.score = 0;
+        this.isOpen = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Boolean getOpen() {
+        return isOpen;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
 }
