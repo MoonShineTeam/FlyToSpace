9b62c2a9f07ffab31f09fdffb53030fb8fd0e590
FlamesYT <dimon12ify@yandex.ru>
2020-12-05 05:36:30 +0500

    Back-end upgrade: xml-parser + easy task manager

--- app/src/main/java/ru/moonshine/flytospace/GameMap.java
+++ app/src/main/java/ru/moonshine/flytospace/GameMap.java
@@ -1,24 +1,30 @@
 package ru.moonshine.flytospace;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import ru.moonshine.flytospace.model.Task;
 import ru.moonshine.flytospace.source.Utils;
 
 public class GameMap extends AppCompatActivity {
+    private ArrayList<Task> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_map);
         Utils.setFullScreenMode(this);
+        XmlResourceParser parser = getResources().getXml(R.xml.tasks);
+        tasks = Utils.readXmlTasks(parser);
     }
 
     public void onLvl1Click(View view) {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
-        view.startAnimation(animation);
+        Utils.startViewAnimation(this, view, R.anim.scale);
+        Utils.intentAnimation(this, MainGameActivity.class, R.anim.fade_in, R.anim.fade_out, tasks.get(0));
     }
 }
---
+++ app/src/main/java/ru/moonshine/flytospace/MainGameActivity.java
@@ -0,0 +1,45 @@
+package ru.moonshine.flytospace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ru.moonshine.flytospace.model.Task;
+import ru.moonshine.flytospace.source.Utils;
+
+public class MainGameActivity extends AppCompatActivity {
+    private int id;
+    private String taskType;
+    private ArrayList<Integer> answers;
+    private ArrayList<String> equations;
+    private String style;
+    private int trueAnswer;
+    private Task task;
+
+    private TextView taskTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Ожидается запуск с передачей данных levelType:int
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        task = (Task) intent.getParcelableExtra("Task");
+        id = task.getId();
+        taskType = task.getTaskType();
+        style = task.getStyle();
+        answers = task.getAnswers();
+        equations = task.getEquations();
+        trueAnswer = task.getTrueAnswer();
+
+        setContentView(R.layout.level_easy_layout);
+        Utils.setFullScreenMode(this);
+        taskTextView = findViewById(R.id.task_easy_textview);
+        taskTextView.setText(task.getTaskText());
+        //TODO: Здесь будет работа с графическими элементами
+
+    }
+}
